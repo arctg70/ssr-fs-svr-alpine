f06cc6f65790d0eb4acee9fac0d128403c301b52
arctg70 <simon.zhou@gmail.com>
2017-03-11 03:45:20 +0800

    Update install_fs_alpine.sh

--- install_fs_alpine.sh
+++ install_fs_alpine.sh
@@ -1,22 +1,17 @@
 #PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 #export PATH
 install_path=/fs/
-package_download_url=https://github.com/ucoker/finalspeed/raw/master/server/finalspeed_server.zip
-package_save_name=finalspeed_server.zip
+#package_download_url=https://github.com/ucoker/finalspeed/raw/master/server/finalspeed_server.zip
+#package_save_name=finalspeed_server.zip
 #  Install finalspeed
-        rm -f $package_save_name
-        echo "Download software..."
-        if ! wget -O $package_save_name $package_download_url ; then
-                echo "Download software failed!"
-                exit 1
-        fi
+#rm -f $package_save_name
+echo "Download software..."
+wget https://github.com/ucoker/finalspeed/raw/master/server/finalspeed_server.zip
         if [[ ! -d "$install_path" ]]; then
                 mkdir "$install_path"
                 else
                 echo "Update Software..."
         fi
-
-        unzip -o $package_save_name  -d $install_path
-
+unzip -o finalspeed_server.zip  -d $install_path
         sh ${install_path}"restart.sh" && exit 0
         tail -f ${install_path}"server.log"
